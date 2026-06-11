306d3e52702bb25d9dc714af3d51ff0cd15ec372
Jamladz <piswap014@gmail.com>
2026-06-11 14:54:23 +0100

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <TonConnectUIProvider 
       manifestUrl={manifestUrl}
       actionsConfiguration={{
-          twaReturnUrl: 'https://t.me/PlushTap_bot',
+          twaReturnUrl: 'https://t.me/PlushTap_bot/app',
           notifications: ['before', 'success'],
           modals: ['before', 'success']
       }}
