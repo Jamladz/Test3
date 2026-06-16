11605ab5dcbee055efff8400c88557a8424d8b3d
Jamladz <piswap014@gmail.com>
2026-06-16 15:38:09 +0100

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,12 @@ import App from './App.tsx';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-const manifestUrl = 'https://raw.githubusercontent.com/Jamladz/Test3/main/tonconnect-manifest.json';
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TonConnectUIProvider 
-      manifestUrl={manifestUrl}
+      manifestUrl="https://jamladz.github.io/Test3/tonconnect-manifest.json"
       actionsConfiguration={{
-          twaReturnUrl: 'https://t.me/PlushTap_bot/app',
-          notifications: ['before', 'success'],
-          modals: ['before', 'success']
+          twaReturnUrl: 'https://t.me/PlushTap_bot/app'
       }}
     >
       <App />
